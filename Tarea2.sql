ad29d4c5b446566cd3129821a207c716c2739661
Imobac Marín <36622131+imobac-marin@users.noreply.github.com>
2018-05-09 11:10:26 +0100

    Add files via upload

--- Tarea2.sql
+++ Tarea2.sql
@@ -1,22 +1,31 @@
 DECLARE
     salario number := round(DBMS_RANDOM.VALUE(600,2000), 2);
+    aumento number;
 BEGIN
     CASE
         WHEN salario <= 600 THEN 
             DBMS_OUTPUT.PUT_LINE('1 - Salario: ' || salario);
-            salario := round(salario * 1.15, 2);
+            aumento := round(salario * 0.15, 2);
+            DBMS_OUTPUT.PUT_LINE('Aumento: ' || aumento);
+            salario := salario + aumento;
             DBMS_OUTPUT.PUT_LINE('Salario después de aumento: ' || salario);
         WHEN salario between 601 and 950 THEN 
             DBMS_OUTPUT.PUT_LINE('2 - Salario: ' || salario);
-            salario := round(salario * 1.135, 2);
+            aumento := round(salario * 0.135, 2);
+            DBMS_OUTPUT.PUT_LINE('Aumento: ' || aumento);
+            salario := salario + aumento;
             DBMS_OUTPUT.PUT_LINE('Salario después de aumento: ' || salario);
         WHEN salario between 951 and 1400 THEN 
             DBMS_OUTPUT.PUT_LINE('3 - Salario: ' || salario);
-            salario := round(salario * 1.1, 2);
+            aumento := round(salario * 0.1, 2);
+            DBMS_OUTPUT.PUT_LINE('Aumento: ' || aumento);
+            salario := salario + aumento;
             DBMS_OUTPUT.PUT_LINE('Salario después de aumento: ' || salario);
         ELSE
             DBMS_OUTPUT.PUT_LINE('4 - Salario: ' || salario);
-            salario := round(salario * 1.05, 2);
+            aumento := round(salario * 0.05, 2);
+            DBMS_OUTPUT.PUT_LINE('Aumento: ' || aumento);
+            salario := salario + aumento;
             DBMS_OUTPUT.PUT_LINE('Salario después de aumento: ' || salario);
       END CASE;
 END;
